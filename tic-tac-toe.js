5077973afe0f78e97d9cd786de2b8e02cedfaa2a
Reynear <douglasreynear@gmail.com>
2025-10-16 18:55:14 -0500

    Completed Activity 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,9 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let moveCount = 0;
     cells.forEach((cell) => {
         cell.classList.add("square");
+        cell.addEventListener("mouseover", () => {
+            cell.classList.add("hover");
+        });
+        cell.addEventListener("mouseout", () => {
+            cell.classList.remove("hover");
+        });
         cell.addEventListener("click", () => {
             if (cell.textContent) return;
-            const symbol = moveCount % 2 === 0 ? "X" : "O";
+            const symbol = moveCount % 2 === 0 ? "X" : "O";    
             cell.textContent = symbol;
             cell.classList.add(symbol);
             moveCount += 1;
